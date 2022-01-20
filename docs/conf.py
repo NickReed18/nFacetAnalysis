e299e7f85d43bd994b510dc623b8eb99b7489cb0
Nicholas Reed <nr1315@ic.ac.uk>
2022-01-20 14:47:30 +0000

    Updates to fix menus

--- docs/conf.py
+++ docs/conf.py
@@ -2,5 +2,16 @@ project = 'nFacet Analysis'
 
 
 extensions = [
-    "nbsphinx",
+    'nbsphinx',
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosommary',
+    'sphinx.ext.intersphinx'
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python/org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+
